caa772c02fcb8668a285318fc87dfab04156afbe
notnotraju <krishnamoorthy@gmail.com>
2023-05-18 16:35:00 +0200

    add a lib.rs file so that unit tests work

---
+++ examples/lib.rs
@@ -0,0 +1,12 @@
+// use clap::Parser;
+// use halo2_scaffold::scaffold::cmd::Cli;
+
+pub mod binary_counting;
+pub mod ipa_rust_simple_hash_batch;
+fn main() {
+    env_logger::init();
+    // let args = Cli::parse();
+    // run(compute_binary_counting, args);
+    // MockProver::run(100, binary_counting,  input);
+    println!("Hello World");
+}
